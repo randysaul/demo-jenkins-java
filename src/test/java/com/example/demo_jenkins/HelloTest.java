577f4b0d60e39c9e349c18f41ff3e88795e7b3f0
randysaul <randy.saul@yahoo.com.mx>
2026-07-27 16:59:18 -0600

    Cambio clase Test

--- src/test/java/com/example/demo_jenkins/HelloTest.java
+++ src/test/java/com/example/demo_jenkins/HelloTest.java
@@ -2,14 +2,12 @@ package com.example.demo_jenkins;
 
 import static org.junit.Assert.assertEquals;
 
-import javax.inject.Inject;
 
 import org.junit.Test;
 
 public class HelloTest {
-	
-	@Inject
-	Hello hola;
+
+	Hello hola = new Hello();
 	
 	String mensaje = hola.sayHello();
 	
